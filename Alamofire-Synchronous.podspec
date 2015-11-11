18c3d3f808b41ca563d6e4fa28094b93933cb8cc
Hran <zhuangluda@gmail.com>
2015-11-11 16:06:36 +0800

    Add source and podspec

---
+++ Alamofire-Synchronous.podspec
@@ -0,0 +1,21 @@
+Pod::Spec.new do |s|
+
+  s.name         = "Alamofire-Synchronous"
+  s.version      = "0.0.1"
+  s.summary      = "Synchronous requests for Alamofire"
+  s.homepage     = "https://github.com/zhuangluda/Alamofire-Synchronous"
+  s.license = 'MIT'
+  s.author = { "Hran" => "zhuangluda@gmail.com" }
+  s.source = { :git => "https://github.com/zhuangluda/Alamofire-Synchronous.git", :tag => s.version }
+
+  s.ios.deployment_target = "8.0"
+  s.osx.deployment_target = "10.9"
+  s.watchos.deployment_target = "2.0"
+  s.tvos.deployment_target = "9.0"
+
+  s.source_files = 'Source/*.swift'
+  s.requires_arc = true
+
+  s.dependency "Alamofire", "~> 3.0"
+
+end
---
+++ Source/Alamofire+Synchronous.swift
@@ -0,0 +1,71 @@
+//
+//  Alamofire+Synchronous.swift
+//  Alamofire-Synchronous
+//
+//  Created by Luda Zhuang on 15/11/8.
+//  Copyright © 2015年 Luda Zhuang. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+extension Request {
+    public func response() -> (request: NSURLRequest?, response: NSHTTPURLResponse?, data: NSData?, error: NSError?) {
+        let semaphore = dispatch_semaphore_create(0)
+        var result: (request: NSURLRequest?, response: NSHTTPURLResponse?, data: NSData?, error: NSError?)!
+        self.response(completionHandler: { request, response, data, error in
+            result = (
+                request: request,
+                response: response,
+                data: data,
+                error: error
+            )
+            dispatch_semaphore_signal(semaphore);
+        })
+        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER)
+        return result
+    }
+    
+    public func responseData() -> Response<NSData, NSError> {
+        let semaphore = dispatch_semaphore_create(0)
+        var result: Response<NSData, NSError>!
+        self.responseData({ response in
+            result = response
+            dispatch_semaphore_signal(semaphore);
+        })
+        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER)
+        return result
+    }
+    
+    public func responseJSON(options options: NSJSONReadingOptions = .AllowFragments) -> Response<AnyObject, NSError> {
+        let semaphore = dispatch_semaphore_create(0)
+        var result: Response<AnyObject, NSError>!
+        self.responseJSON(options: options, completionHandler: {response in
+            result = response
+            dispatch_semaphore_signal(semaphore);
+        })
+        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER)
+        return result
+    }
+    
+    public func responseString(encoding encoding: NSStringEncoding? = nil) -> Response<String, NSError> {
+        let semaphore = dispatch_semaphore_create(0)
+        var result: Response<String, NSError>!
+        self.responseString(encoding: encoding, completionHandler: { response in
+            result = response
+            dispatch_semaphore_signal(semaphore);
+        })
+        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER)
+        return result
+    }
+    
+    public func responsePropertyList(options options: NSPropertyListReadOptions = NSPropertyListReadOptions()) -> Response<AnyObject, NSError> {
+        let semaphore = dispatch_semaphore_create(0)
+        var result: Response<AnyObject, NSError>!
+        self.responsePropertyList(options: options, completionHandler: { response in
+            result = response
+            dispatch_semaphore_signal(semaphore);
+        })
+        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER)
+        return result
+    }
+}
